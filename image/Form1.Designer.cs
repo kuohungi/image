b7aa52406152766d23ee6024c8bff754ac672377
kuohungi <79037597+kuohungi@users.noreply.github.com>
2021-03-10 13:44:17 +0800

    加入專案檔案。

---
+++ image/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFile = new System.Windows.Forms.Button();
+            this.original = new System.Windows.Forms.Button();
+            this.gray = new System.Windows.Forms.Button();
+            this.red = new System.Windows.Forms.Button();
+            this.green = new System.Windows.Forms.Button();
+            this.blue = new System.Windows.Forms.Button();
+            this.turn = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.value_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(45, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(380, 442);
+            this.panel1.TabIndex = 0;
+            // 
+            // openFile
+            // 
+            this.openFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(160)))));
+            this.openFile.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.openFile.Location = new System.Drawing.Point(478, 45);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(326, 54);
+            this.openFile.TabIndex = 1;
+            this.openFile.Text = "開啟圖檔";
+            this.openFile.UseVisualStyleBackColor = false;
+            this.openFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // original
+            // 
+            this.original.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.original.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.original.Location = new System.Drawing.Point(655, 141);
+            this.original.Name = "original";
+            this.original.Size = new System.Drawing.Size(149, 54);
+            this.original.TabIndex = 0;
+            this.original.Text = "原圖";
+            this.original.UseVisualStyleBackColor = false;
+            this.original.Click += new System.EventHandler(this.Original_Click);
+            // 
+            // gray
+            // 
+            this.gray.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.gray.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.gray.Location = new System.Drawing.Point(655, 225);
+            this.gray.Name = "gray";
+            this.gray.Size = new System.Drawing.Size(149, 54);
+            this.gray.TabIndex = 2;
+            this.gray.Text = "灰階";
+            this.gray.UseVisualStyleBackColor = false;
+            this.gray.Click += new System.EventHandler(this.Gray_Click);
+            // 
+            // red
+            // 
+            this.red.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.red.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.red.Location = new System.Drawing.Point(478, 141);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(149, 54);
+            this.red.TabIndex = 3;
+            this.red.Text = "紅階";
+            this.red.UseVisualStyleBackColor = false;
+            this.red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // green
+            // 
+            this.green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(240)))), ((int)(((byte)(190)))));
+            this.green.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.green.Location = new System.Drawing.Point(478, 225);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(149, 54);
+            this.green.TabIndex = 4;
+            this.green.Text = "綠階";
+            this.green.UseVisualStyleBackColor = false;
+            this.green.Click += new System.EventHandler(this.Green_Click);
+            // 
+            // blue
+            // 
+            this.blue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(230)))), ((int)(((byte)(255)))));
+            this.blue.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.blue.Location = new System.Drawing.Point(478, 309);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(149, 54);
+            this.blue.TabIndex = 5;
+            this.blue.Text = "藍階";
+            this.blue.UseVisualStyleBackColor = false;
+            this.blue.Click += new System.EventHandler(this.Blue_Click);
+            // 
+            // turn
+            // 
+            this.turn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(189)))), ((int)(((byte)(240)))));
+            this.turn.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.turn.Location = new System.Drawing.Point(655, 309);
+            this.turn.Name = "turn";
+            this.turn.Size = new System.Drawing.Size(149, 54);
+            this.turn.TabIndex = 6;
+            this.turn.Text = "反轉";
+            this.turn.UseVisualStyleBackColor = false;
+            this.turn.Click += new System.EventHandler(this.Turn_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(478, 401);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(326, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(545, 458);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 28);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "閾值：";
+            // 
+            // value_label
+            // 
+            this.value_label.AutoSize = true;
+            this.value_label.Font = new System.Drawing.Font("微軟正黑體", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.value_label.ForeColor = System.Drawing.Color.White;
+            this.value_label.Location = new System.Drawing.Point(650, 458);
+            this.value_label.Name = "value_label";
+            this.value_label.Size = new System.Drawing.Size(50, 28);
+            this.value_label.TabIndex = 9;
+            this.value_label.Text = "null";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(70)))), ((int)(((byte)(109)))));
+            this.ClientSize = new System.Drawing.Size(862, 532);
+            this.Controls.Add(this.value_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.green);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.turn);
+            this.Controls.Add(this.blue);
+            this.Controls.Add(this.red);
+            this.Controls.Add(this.gray);
+            this.Controls.Add(this.original);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "影像處理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button original;
+        private System.Windows.Forms.Button gray;
+        private System.Windows.Forms.Button red;
+        private System.Windows.Forms.Button green;
+        private System.Windows.Forms.Button blue;
+        private System.Windows.Forms.Button turn;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label value_label;
+    }
+}
+
---
+++ image/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap pic;
+        Bitmap origPic;
+        Bitmap grayPic;
+        Bitmap redPic;
+        Bitmap greenPic;
+        Bitmap bluePic;
+        Bitmap turnPic;
+        public int rgb;
+        public int controlTrack = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            value_label.Text = "null";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //trackBar歸位
+                controlTrack = 1;
+                trackBar1.Value = 0;
+                controlTrack = 0;
+
+                //開啟的圖檔存在pic
+                pic = new Bitmap(openFileDialog1.FileName);
+                panel1.BackgroundImage = pic;
+                panel1.BackgroundImageLayout = ImageLayout.Zoom;
+                panel1.Refresh();
+
+                rgb = 0;
+                int H = pic.Height;
+                int W = pic.Width;
+                origPic = new Bitmap(W, H);
+                grayPic = new Bitmap(W, H);
+                redPic = new Bitmap(W, H);
+                greenPic = new Bitmap(W, H);
+                bluePic = new Bitmap(W, H);
+                turnPic = new Bitmap(W, H);
+                origPic = pic;
+
+                for (int y = 0; y < H; ++y)
+                {
+                    for (int x = 0; x < W; ++x)
+                    {
+                        Color picrgb = pic.GetPixel(x, y);
+                        int R = picrgb.R;
+                        int G = picrgb.G;
+                        int B = picrgb.B;
+                        int gray = (R + G + B) / 3;
+                        Color ggray = Color.FromArgb(gray, gray, gray);
+                        grayPic.SetPixel(x, y, ggray);
+                        Color rred = Color.FromArgb(R, 0, 0);
+                        redPic.SetPixel(x, y, rred);
+                        Color ggren = Color.FromArgb(0, G, 0);
+                        greenPic.SetPixel(x, y, ggren);
+                        Color bblue = Color.FromArgb(0, 0, B);
+                        bluePic.SetPixel(x, y, bblue);
+                        Color tturn = Color.FromArgb(255 - R, 255 - G, 255 - B);
+                        turnPic.SetPixel(x, y, tturn);
+                    }
+                }
+            }
+        }
+
+        private void Original_Click(object sender, EventArgs e)
+        {
+            rgb = 5;
+            panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            panel1.BackgroundImage = pic;
+            value_label.Text = "null";
+            controlTrack = 1;
+            trackBar1.Value = 0;
+            controlTrack = 0;
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            value_label.Text = "" + trackBar1.Value;
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (controlTrack == 0)
+            {
+                Bitmap trackPic = new Bitmap(pic.Width, pic.Height);
+                Color tra;
+                for (int y = 0; y < trackPic.Height; ++y)
+                {
+                    for (int x = 0; x < trackPic.Width; ++x)
+                    {
+                        switch (rgb)
+                        {
+                            case 0:
+                                tra = grayPic.GetPixel(x, y);
+                                break;
+                            case 1:
+                                tra = redPic.GetPixel(x, y);
+                                break;
+                            case 2:
+                                tra = greenPic.GetPixel(x, y);
+                                break;
+                            case 3:
+                                tra = bluePic.GetPixel(x, y);
+                                break;
+                            case 4:
+                                tra = turnPic.GetPixel(x, y);
+                                break;
+                            case 5:
+                                tra = origPic.GetPixel(x, y);
+                                break;
+                            default:
+                                tra = grayPic.GetPixel(x, y);
+                                break;
+                        }
+
+                        int R = tra.R;
+                        int G = tra.G;
+                        int B = tra.B;
+                        if (R >= trackBar1.Value)
+                            R = 255;
+                        else
+                            R = 0;
+                        if (G >= trackBar1.Value)
+                            G = 255;
+                        else
+                            G = 0;
+                        if (B >= trackBar1.Value)
+                            B = 255;
+                        else B = 0;
+                        Color trat = Color.FromArgb(R, G, B);
+                        trackPic.SetPixel(x, y, trat);
+                    }
+                }
+                panel1.BackgroundImageLayout = ImageLayout.Zoom;
+                panel1.BackgroundImage = trackPic;
+            }
+        }
+
+        private void Gray_Click(object sender, EventArgs e)
+        {
+            rgb = 0;
+            panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            panel1.BackgroundImage = grayPic;
+            value_label.Text = "null";
+            controlTrack = 1;
+            trackBar1.Value = 0;
+            controlTrack = 0;
+        }
+
+        private void Red_Click(object sender, EventArgs e)
+        {
+            rgb = 1;
+            panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            panel1.BackgroundImage = redPic;
+            value_label.Text = "null";
+            controlTrack = 1;
+            trackBar1.Value = 0;
+            controlTrack = 0;
+        }
+
+        private void Green_Click(object sender, EventArgs e)
+        {
+            rgb = 2;
+            panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            panel1.BackgroundImage = greenPic;
+            value_label.Text = "null";
+            controlTrack = 1;
+            trackBar1.Value = 0;
+            controlTrack = 0;
+        }
+
+        private void Blue_Click(object sender, EventArgs e)
+        {
+            rgb = 3;
+            panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            panel1.BackgroundImage = bluePic;
+            value_label.Text = "null";
+            controlTrack = 1;
+            trackBar1.Value = 0;
+            controlTrack = 0;
+        }
+
+        private void Turn_Click(object sender, EventArgs e)
+        {
+            rgb = 4;
+            panel1.BackgroundImageLayout = ImageLayout.Zoom;
+            panel1.BackgroundImage = turnPic;
+            value_label.Text = "null";
+            controlTrack = 1;
+            trackBar1.Value = 0;
+            controlTrack = 0;
+        }
+    }
+}
